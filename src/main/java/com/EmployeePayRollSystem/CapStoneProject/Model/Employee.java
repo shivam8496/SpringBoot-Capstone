93f7ff46642e589ddf48d6e062eb9fdf55d3a6bc
shivamsingh8496 <shivamsingh8496@gmail.com>
2025-07-02 18:42:37 +0000

    testing changes firebase

--- src/main/java/com/EmployeePayRollSystem/CapStoneProject/Model/Employee.java
+++ src/main/java/com/EmployeePayRollSystem/CapStoneProject/Model/Employee.java
@@ -4,6 +4,7 @@ package com.EmployeePayRollSystem.CapStoneProject.Model;
 import jakarta.persistence.*;
 
 // Changes from Local PC
+//Changes from FireBase
 @Entity
 public class Employee {
     @Id
